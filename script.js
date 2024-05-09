f7537336cfe0363e5c551529e56fe05a87a307c4
Zohair1130 <157407522+Zohair1130@users.noreply.github.com>
2024-05-09 15:56:50 -0400

    better webpage formatting and extra text

--- script.js
+++ script.js
@@ -18,3 +18,8 @@ function scrollFunction() {
     document.getElementById("navbar").style.top = "0px";
   }
 } 
+
+var siteWidth = 1280;
+var scale = screen.width /siteWidth;
+
+document.querySelector('meta[name="viewport"]').setAttribute('content', 'width='+siteWidth+', initial-scale='+scale+'');
